8ee1b9281d2dfa76385ce9aa02da3fb865f93688
c00c <c00c@c00c.cc>
2018-06-29 10:59:53 +0800

    Create update_kernel.sh

---
+++ update_kernel.sh
@@ -0,0 +1,5 @@
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.18-rc2/linux-headers-4.18.0-041800rc2_4.18.0-041800rc2.201806241430_all.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.18-rc2/linux-headers-4.18.0-041800rc2-generic_4.18.0-041800rc2.201806241430_amd64.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.18-rc2/linux-image-unsigned-4.18.0-041800rc2-generic_4.18.0-041800rc2.201806241430_amd64.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.18-rc2/linux-modules-4.18.0-041800rc2-generic_4.18.0-041800rc2.201806241430_amd64.deb
+dpkg -i *.deb
